b6f56e5561c287305225fb84b9e01776727aeae7
Kurt Mueller <kurtronaldmueller@gmail.com>
2020-07-19 13:03:07 -0400

    Create body measurement module

---
+++ src/BodyMeasurement.elm
@@ -0,0 +1,20 @@
+module BodyMeasurement exposing (BodyMeasurement, bodyFatMass, leanMusclessMass)
+
+import Mass exposing (Mass)
+import Percentage exposing (Percentage)
+
+
+type alias BodyMeasurement =
+    { weight : Mass
+    , bodyFatPercentage : Percentage
+    }
+
+
+bodyFatMass : BodyMeasurement -> Mass
+bodyFatMass { weight, bodyFatPercentage } =
+    Mass.mapPercentage (*) weight bodyFatPercentage
+
+
+leanMusclessMass : BodyMeasurement -> Mass
+leanMusclessMass ({ weight } as bodyMeasurement) =
+    Mass.map2 (-) weight (bodyFatMass bodyMeasurement)
