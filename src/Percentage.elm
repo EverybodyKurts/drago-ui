4db7b9f150bc747519416953b65e35c6b6674ab3
Kurt Mueller <kurtronaldmueller@gmail.com>
2020-07-19 13:01:08 -0400

    Convert a percentage to a decimal

--- src/Percentage.elm
+++ src/Percentage.elm
@@ -1,4 +1,4 @@
-module Percentage exposing (Percentage, fromFloat, map2)
+module Percentage exposing (Percentage, fromFloat, map2, toDecimal)
 
 
 type Percentage
@@ -15,3 +15,13 @@ fromFloat =
 map2 : (Float -> Float -> Float) -> Percentage -> Percentage -> Percentage
 map2 fn (Percentage pct1) (Percentage pct2) =
     Percentage (fn pct1 pct2)
+
+
+{-| Convert a percentage to decimal.
+
+    toDecimal (Percentage 30.0) == 0.3
+
+-}
+toDecimal : Percentage -> Float
+toDecimal (Percentage pct) =
+    pct * 0.01
