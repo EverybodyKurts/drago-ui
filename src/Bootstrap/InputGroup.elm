1a336d708070aa18e8f53b1eb56a506eb63e0e2b
Kurt Mueller <kurtronaldmueller@gmail.com>
2020-07-20 20:56:14 -0400

    Create inputGroup

--- src/Bootstrap/InputGroup.elm
+++ src/Bootstrap/InputGroup.elm
@@ -1,7 +1,12 @@
-module Bootstrap.InputGroup exposing (append)
+module Bootstrap.InputGroup exposing (append, inputGroup)
 
 import Html exposing (Html, div)
 import Html.Attributes exposing (class)
+
+
+inputGroup : List (Html msg) -> Html msg
+inputGroup =
+    div [ class "input-group" ]
 
 
 append : List (Html msg) -> Html msg
