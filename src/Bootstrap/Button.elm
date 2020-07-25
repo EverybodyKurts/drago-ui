f3452ec897255bc2380426e450e973dbfca38b13
Kurt Mueller <kurtronaldmueller@gmail.com>
2020-07-24 23:27:19 -0400

    Update outline secondary button

--- src/Bootstrap/Button.elm
+++ src/Bootstrap/Button.elm
@@ -1,15 +1,16 @@
 module Bootstrap.Button exposing (outlineSecondary)
 
 import Html exposing (Html, button)
-import Html.Attributes exposing (class, id, type_)
+import Html.Attributes exposing (class, id, type_, value)
 import Html.Events exposing (onClick)
 
 
-outlineSecondary : msg -> String -> List (Html msg) -> Html msg
-outlineSecondary clickMsg htmlId =
+outlineSecondary : msg -> String -> Html msg -> Html msg
+outlineSecondary clickMsg htmlId buttonValue =
     button
         [ class "btn btn-outline-secondary"
         , type_ "button"
         , id htmlId
         , onClick clickMsg
         ]
+        [ buttonValue ]
