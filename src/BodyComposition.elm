56b7d9808d3ffdcc208ad48887dcdd828bbbebde
Kurt Mueller <kurtronaldmueller@gmail.com>
2020-08-01 10:23:30 -0400

    Calculate basal metabolic rate

--- src/BodyComposition.elm
+++ src/BodyComposition.elm
@@ -1,20 +1,36 @@
-module BodyComposition exposing (BodyMeasurement, bodyFatMass, leanMusclessMass)
+module BodyComposition exposing (BodyComposition, basalMetabolicRate, bodyFatMass, leanMusclessMass)
 
+import Calories exposing (Calories)
 import Mass exposing (Mass)
 import Percentage exposing (Percentage)
 
 
-type alias BodyMeasurement =
+type alias BodyComposition =
     { weight : Mass
     , bodyFatPercentage : Percentage
     }
 
 
-bodyFatMass : BodyMeasurement -> Mass
+bodyFatMass : BodyComposition -> Mass
 bodyFatMass { weight, bodyFatPercentage } =
     Mass.mapPercentage (*) weight bodyFatPercentage
 
 
-leanMusclessMass : BodyMeasurement -> Mass
-leanMusclessMass ({ weight } as bodyMeasurement) =
-    Mass.map2 (-) weight (bodyFatMass bodyMeasurement)
+leanMusclessMass : BodyComposition -> Mass
+leanMusclessMass ({ weight } as bodyComposition) =
+    Mass.map2 (-) weight (bodyFatMass bodyComposition)
+
+
+{-| Calculate a person's basal metabolic rate based on their lean muscle mass. Uses the Katch-McCardle formula.
+-}
+basalMetabolicRate : BodyComposition -> Calories
+basalMetabolicRate bodyComposition =
+    let
+        lmm =
+            leanMusclessMass bodyComposition
+    in
+    if Mass.isKg lmm then
+        Calories.fromFloat (370 + (21.6 * Mass.toFloat lmm))
+
+    else
+        Calories.fromFloat (370 + (9.7976 * Mass.toFloat lmm))
